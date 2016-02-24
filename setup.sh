04e1002be422d40d318f9277046d3007d47a22f5
NT Blok <ntedeschiblok@gmail.com>
2016-02-24 15:10:45 -0800

    Updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+## Part 1
+# Create a project (e.g., collection of configuration and apps for a website)
 django-admin startproject mysite
 
+# Create an app (e.g., web application within a website)
 cd mysite
 python manage.py startapp polls
+
+## Part 2
+# Create tables in the database
+python manage.py migrate
