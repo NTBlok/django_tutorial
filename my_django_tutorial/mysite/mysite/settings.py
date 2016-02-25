b5e34f1eb77357b9385a9142d9a3fec5944622e0
NT Blok <ntedeschiblok@gmail.com>
2016-02-25 04:17:56 +0000

    Edited

--- my_django_tutorial/mysite/mysite/settings.py
+++ my_django_tutorial/mysite/mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import secretKey as sk  #Added to obscure secret key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = sk.SECRETKEY #Stored in a seperate file for security
+SECRET_KEY = os.environ.get('SECRETKEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
