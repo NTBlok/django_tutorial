a1302302348139d6026f2abe213e90c647a9df6b
NT Blok <ntedeschiblok@gmail.com>
2016-02-25 12:54:16 -0800

    Update

--- part_2.sh
+++ part_2.sh
@@ -1,18 +1,18 @@
 ## Part 2
 # Create initial tables in the database
-python manage.py migrate
+python mysite/manage.py migrate
 
 # Store changes in models in files on disk (e.g., polls/migrations/0001_initial.py)
-python manage.py makemigrations polls
+python mysite/manage.py makemigrations polls
 
 
 ## Optional BEGIN
 # Return SQL run by migration
-python manage.py sqlmigrate polls 0001
+python mysite/manage.py sqlmigrate polls 0001
 # Use check command to look for problems without making migrations
-python manage.py check
+python mysite/manage.py check
 ## Optional END
 
 
 # Re-run to create tables in the database by applying all migrations to synchronize changes
-python manage.py migrate
+python mysite/manage.py migrate
