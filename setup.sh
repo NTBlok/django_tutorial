a74a939eb4990572232c303cb1458a9e3641cb36
NT Blok <ntedeschiblok@gmail.com>
2016-02-29 13:45:54 -0800

    Updated

--- setup.sh
+++ setup.sh
@@ -20,29 +20,34 @@ sudo pip install Django==1.9.2
 sudo apt-get install tree
 
 # Vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir -p ~/.vim/autoload ~/.vim/bundle;
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-echo "execute pathogen#infect()" >> ~/.vimrc
-echo "syntax on" >> ~/.vimrc
-echo "filetype plugin indent on" >> ~/.vimrc
-echo "syntax enable" >> ~/.vimrc
-echo "set number" >> ~/.vimrc
-echo "set ts=4" >> ~/.vimrc
-echo "set autoindent" >> ~/.vimrc
-echo "set expandtab" >> ~/.vimrc
-echo "set shiftwidth=4" >> ~/.vimrc
-echo "set cursorline" >> ~/.vimrc
-echo "set showmatch" >> ~/.vimrc
-echo "let python_highlight_all = 1" >> ~/.vimrc
-#echo "Plugin 'mxw/vim-jsx'" >> ~/.vimrc
-cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-sensible.git
-git clone https://github.com/scrooloose/nerdtree.git
-sudo apt-get install exuberant-ctags
-git clone git://github.com/majutsushi/tagbar
-sudo pip install --user jedi
-git clone https://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# Build .vimrc
+if [ -e ~/.vimrc ]; then mv ~/.vimrc ~/.vimrc_bak; fi
+echo "set nocompatible"             >> ~/.vimrc
+echo "filetype plugin indent off"       >> ~/.vimrc
+echo "syntax off"               >> ~/.vimrc
+echo "set rtp+=~/.vim/bundle/Vundle.vim"    >> ~/.vimrc
+echo "call vundle#begin()"          >> ~/.vimrc
+echo "Plugin 'gmarik/Vundle.vim'"       >> ~/.vimrc
+echo "Plugin 'The-NERD-tree'"           >> ~/.vimrc
+echo "Plugin 'mxw/vim-jsx'"         >> ~/.vimrc
+echo "Plugin 'davidhalter/jedi-vim'"        >> ~/.vimrc
+echo "Plugin 'majutsushi/tagbar'"       >> ~/.vimrc
+echo "Plugin 'pangloss/vim-javascript'"     >> ~/.vimrc
+echo "call vundle#end()"            >> ~/.vimrc
+echo "filetype plugin indent on"        >> ~/.vimrc
+echo "syntax on"                >> ~/.vimrc
+echo "let g:jsx_ext_required = 0"       >> ~/.vimrc
+echo "let python_highlight_all = 1"         >> ~/.vimrc
+echo "set number"               >> ~/.vimrc
+echo "set ts=4"                 >> ~/.vimrc
+echo "set autoindent"               >> ~/.vimrc
+echo "set expandtab"                >> ~/.vimrc
+echo "set shiftwidth=4"             >> ~/.vimrc
+echo "set cursorline"               >> ~/.vimrc
+echo "set showmatch"                >> ~/.vimrc
+# To Finish Vim Plugin Installation, Run :PluginInstall inside vim
 
 # Dotfiles
 git clone https://github.com/startup-class/dotfiles.git
